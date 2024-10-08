afd0f9b1f5d89c7c9276202d9960fd3256631641
Silver Kurrik <kurriksilver@gmail.com>
2024-10-08 17:58:30 +0300

    File size validation

--- src/main/java/com/example/telemaapi/infrastructure/GlobalExceptionHandler.java
+++ src/main/java/com/example/telemaapi/infrastructure/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
@@ -18,6 +19,7 @@ import jakarta.validation.ConstraintViolationException;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler({ HandlerMethodValidationException.class, ConstraintViolationException.class })
+	@ResponseBody
 	public ResponseEntity<ApiError> handleValidationExceptions(Exception e) {
 		List<String> errors = new ArrayList<>();
 
@@ -37,6 +39,7 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	@ResponseBody
 	public ResponseEntity<ApiError> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
 		String message = "File size exceeds the maximum allowed size (10MB)";
 
